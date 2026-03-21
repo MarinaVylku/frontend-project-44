4f9d2d20a99bc3eefbfc6763d00e5e2e76b12763
MarinaVylku <vmarinav14011990@gmail.com>
2026-03-21 20:09:20 +0300

    step six

---
+++ src/games/calc.js
@@ -0,0 +1,40 @@
+import { getRandomNumber } from '../utils.js'
+
+import runGameLogic from './gameLogic.js'
+
+const description = 'What is the result of the expression?'
+
+const getRandomOperator = () => {
+  const signes = ['+', '-', '*']
+  const random = getRandomNumber(0, 2)
+  return signes[random]
+}
+
+const generateRounds = () => {
+  const rounds = []
+  for (let i = 0; i < 2; i += 1) {
+    const firstOperand = getRandomNumber(1, 100)
+    const secondOperand = getRandomNumber(1, 100)
+    const operator = getRandomOperator()
+
+    const question = `${firstOperand} ${operator} ${secondOperand}`
+    let correctAnswer
+    if (operator === '+') {
+      correctAnswer = String(firstOperand + secondOperand)
+    }
+    else if (operator === '-') {
+      correctAnswer = String(firstOperand - secondOperand)
+    }
+    else {
+      correctAnswer = String(firstOperand * secondOperand)
+    }
+    rounds.push([question, correctAnswer])
+  }
+  return rounds
+}
+
+const runCalcGame = () => {
+  const rounds = generateRounds()
+  runGameLogic(rounds, description)
+}
+export default runCalcGame
--- src/games/even.js
+++ src/games/even.js
@@ -1,4 +1,4 @@
-import getRandomNumber from '../utils.js'
+import { getRandomNumber } from '../utils.js'
 import runGameLogic from './gameLogic.js'
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".'
