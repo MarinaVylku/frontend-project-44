4d4623cce50d8448447d368c90523fa0813a01b2
Marina <vmarinav14011990@gmail.com>
2026-06-09 19:55:06 +0300

    Update gameLogic.js

--- src/games/gameLogic.js
+++ src/games/gameLogic.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync'
 
 const runGameLogic = (generateRound, description) => {
   console.log('Welcome to the Brain Games!')
-  const userName = readlineSync.question('May i have your name? ')
+  const userName = readlineSync.question('May I have your name? ')
   console.log(`Hello, ${userName}`)
   console.log(description)
   for (let i = 0; i < 3; i += 1) {
@@ -10,7 +10,8 @@ const runGameLogic = (generateRound, description) => {
     console.log(`Question: ${question}`)
     const userAnswer = readlineSync.question('Your answer: ')
     if (userAnswer !== correctAnswer) {
-      console.log(`'${userAnswer}' is wrong answer ; ). Correct answer was '${correctAnswer}'`)
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`)
+      console.log(`Let's try again, ${userName}!`)
       return
     }
     console.log('Correct!')
